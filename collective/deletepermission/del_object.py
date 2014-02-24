7a1ef3c89b0fc39b51b630fdbdcd049c67db1b77
Jonas Baumann <jone@jone.ch>
2014-02-24 15:15:13 +0100

    Fix protection of manage_delObjects with "Delete objects".
    In certain situations, when only having the permission to delete the
    content ("Delete portal content") but not the permission to delete from the
    the parent container ("Delete objects" on the parent) deleting was possible
    even though it shouldn't have been.
    
    This should not be that problematic since the user alyways needed the
    "Delete portal content" permission on the item.

--- collective/deletepermission/del_object.py
+++ collective/deletepermission/del_object.py
@@ -1,17 +1,21 @@
-from Products.CMFCore.PortalFolder import PortalFolderBase as PortalFolder
 from AccessControl import Unauthorized
 from AccessControl import getSecurityManager
+from Products.CMFCore.PortalFolder import PortalFolderBase as PortalFolder
 
 
 def manage_delObjects(self, ids=None, REQUEST=None):
     """We need to enforce security."""
+    sm = getSecurityManager()
+    if not sm.checkPermission('Delete objects', self):
+        raise Unauthorized(
+            "Do not have permissions to remove this object")
+
     if ids is None:
         ids = []
     if isinstance(ids, basestring):
         ids = [ids]
     for id_ in ids:
         item = self._getOb(id_)
-        sm = getSecurityManager()
         if not sm.checkPermission("Delete portal content", item):
             raise Unauthorized(
                 "Do not have permissions to remove this object")
